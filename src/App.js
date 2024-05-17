f9ad1ccfccb75f55d1eb586c03f1a47056cef85f
Dattatraya K <GithubDat@users.noreply.github.com>
2024-05-17 11:03:26 +0530

    Update App.js
    Spelling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import useFetch from "./customhooks/useFetch";
 import './App.css';
 
 const App = () => {
-  const { resposnseFromUrl, isLoading, isError, apiError } = useFetch('https://dummyjson.com/users');
-  console.log('Comp DATA', resposnseFromUrl);
+  const { responseFromUrl, isLoading, isError, apiError } = useFetch('https://dummyjson.com/users');
+  console.log('Comp DATA', responseFromUrl);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -18,7 +18,7 @@ const App = () => {
     <main>
       <section className="menu-section">
         <h1>API Data</h1>
-        {resposnseFromUrl.users.map((res) => (
+        {responseFromUrl.users.map((res) => (
           <div key={res.id}>
             <span className='user-detail'>{res.firstName} {res.lastName}</span>
             <span className='user-detail--email'>{res.email}</span>
